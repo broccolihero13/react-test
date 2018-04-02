7fa353a73eb893d51d9a86b1693bf9eaf8e9eb8f
Brock Halladay <bhalladay@instructure.com>
2018-04-02 13:34:13 -0600

    added interval to get the timer to count when the page is loaded

--- week_three/build/index.js
+++ week_three/build/index.js
@@ -6,8 +6,28 @@ class PomodoroTimer extends React.Component {
   getTotalTime() {
     return this.props.workingTime + this.props.restingTime;
   }
+
+  componentDidMount() {
+    // let startTimer = new Date();
+    this.interval = setInterval(this.countSeconds.bind(this), 1000);
+    this.setState({ start: new Date() });
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
   countSeconds() {
-    setInterval(function () {}, 1000);
+    let currentTime = new Date();
+    let timeElapsed = Math.floor((currentTime - this.state.start) / 1000);
+    this.setState({ timeElapsed: timeElapsed });
+    if (timeElapsed == this.props.workingTime * 60) {
+      alert('Working time completed, please begin rest time for 5 minutes');
+    }
+    if (timeElapsed == this.props.workingTime * 60 + this.props.restingTime * 60) {
+      alert('Rest time completed');
+      clearInterval(this.interval);
+    }
   }
   render() {
     return React.createElement(
