03dbce2824c31f89887b754e1d8ebdd78f56c904
Brock Halladay <bhalladay@instructure.com>
2018-04-02 12:43:01 -0600

    Basic structure for Pomodoro Timer

---
+++ week_three/app/index.js
@@ -0,0 +1,8 @@
+class PomodoroTimer extends React.Component {
+  render(){
+    return(
+      <h1>Hello Hello! I don't know why you say goodbye; I say hello</h1>
+    );
+  }
+}
+ReactDOM.render(<PomodoroTimer />,document.getElementById('app'));
---
+++ week_three/build/index.js
@@ -0,0 +1,10 @@
+class PomodoroTimer extends React.Component {
+  render() {
+    return React.createElement(
+      'h1',
+      null,
+      'Hello Hello! I don\'t know why you say goodbye; I say hello'
+    );
+  }
+}
+ReactDOM.render(React.createElement(PomodoroTimer, null), document.getElementById('app'));
